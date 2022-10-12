6557467eca1230bbab124f4e668a2f5cb7358d9f
NicoA8 <nicoastancai@gmail.com>
2022-10-12 15:18:08 +0300

    AddAnimationAboutPage.

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Head from "next/head";
 
 // STYLES
@@ -20,7 +20,49 @@ import ExpBox from "../../components/ExpBox";
 import AbilityList from "../../components/AbilityList";
 import SkillsBar from "../../components/SkillsBar";
 
+function elementIsInView(myRef, setState) {
+  useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+      const entry = entries[0];
+      setState(entry.isIntersecting);
+    });
+    observer.observe(myRef.current);
+  }, []);
+}
+
 function MyAboutPage() {
+  const informationSection = useRef();
+  const [informationIsVisible, setInformationIsVisible] = useState();
+  elementIsInView(informationSection, setInformationIsVisible);
+
+  const schoolSection = useRef();
+  const [schoolIsVisible, setSchoolIsVisible] = useState();
+  elementIsInView(schoolSection, setSchoolIsVisible);
+
+  const jobsSection = useRef();
+  const [jobsIsVisible, setJobsIsVisible] = useState();
+  elementIsInView(jobsSection, setJobsIsVisible);
+
+  const languagesSection = useRef();
+  const [languagesIsVisible, setLanguageIsVisible] = useState();
+  elementIsInView(languagesSection, setLanguageIsVisible);
+
+  const digitalsSection = useRef();
+  const [digitalsIsVisible, setDigitalsIsVisible] = useState();
+  elementIsInView(digitalsSection, setDigitalsIsVisible);
+
+  const orgsSection = useRef();
+  const [orgsIsVisible, setOrgsIsVisible] = useState();
+  elementIsInView(orgsSection, setOrgsIsVisible);
+
+  const comsSection = useRef();
+  const [comsIsVisible, setComsIsVisible] = useState();
+  elementIsInView(comsSection, setComsIsVisible);
+
+  const profsSection = useRef();
+  const [profsIsVisible, setProfsIsVisible] = useState();
+  elementIsInView(profsSection, setProfsIsVisible);
+
   return (
     <>
       <Head>
@@ -35,7 +77,12 @@ function MyAboutPage() {
             <p className={styles.sub_title}>arhitect</p>
           </div>
         </header>
-        <section className={styles.information}>
+        <section
+          className={`${styles.information} ${
+            informationIsVisible ? styles.elementInView : ""
+          }`}
+          ref={informationSection}
+        >
           {informations.map(information => (
             <InfoBox
               key={information.id}
@@ -49,7 +96,12 @@ function MyAboutPage() {
             />
           ))}
         </section>
-        <section className={styles.school}>
+        <section
+          className={`${styles.school} ${
+            schoolIsVisible ? styles.elementInView : ""
+          }`}
+          ref={schoolSection}
+        >
           <p className={styles.section_title}>
             educație și formare profesională
           </p>
@@ -63,7 +115,12 @@ function MyAboutPage() {
             />
           ))}
         </section>
-        <section className={styles.jobs}>
+        <section
+          className={`${styles.jobs} ${
+            jobsIsVisible ? styles.elementInView : ""
+          }`}
+          ref={jobsSection}
+        >
           <p className={styles.section_title}>experiență profesională</p>
           {jobs.map(jobs => (
             <ExpBox
@@ -75,11 +132,21 @@ function MyAboutPage() {
             />
           ))}
         </section>
-        <section className={styles.languages}>
+        <section
+          className={`${styles.languages} ${
+            languagesIsVisible ? styles.elementInView : ""
+          }`}
+          ref={languagesSection}
+        >
           <p className={styles.section_title}>competențe lingvistice</p>
           <AbilityList abilities={languages} />
         </section>
-        <section className={styles.digitals}>
+        <section
+          className={`${styles.digitals} ${
+            digitalsIsVisible ? styles.elementInView : ""
+          }`}
+          ref={digitalsSection}
+        >
           <p className={styles.section_title}>competențe digitale</p>
           <ul className={styles.abilities_list}>
             <li className={`${styles.list_item} ${styles.autodesk}`}>
@@ -119,17 +186,32 @@ function MyAboutPage() {
             <SkillsBar level={5} />
           </ul>
         </section>
-        <section className={styles.orgs}>
+        <section
+          className={`${styles.orgs} ${
+            orgsIsVisible ? styles.elementInView : ""
+          }`}
+          ref={orgsSection}
+        >
           <p className={styles.section_title}>competențe organizatorice</p>
           <AbilityList abilities={orgs} />
         </section>
-        <section className={styles.coms}>
+        <section
+          className={`${styles.coms} ${
+            comsIsVisible ? styles.elementInView : ""
+          }`}
+          ref={comsSection}
+        >
           <p className={styles.section_title}>
             competențe de comunicare. interpersonale
           </p>
           <AbilityList abilities={coms} />
         </section>
-        <section className={styles.profs}>
+        <section
+          className={`${styles.profs} ${
+            profsIsVisible ? styles.elementInView : ""
+          }`}
+          ref={profsSection}
+        >
           <p className={styles.section_title}>
             competențe dobândite la locul de muncă
           </p>
