992e012e6ff1ebc44af3d0095bda306553bc272a
NicoA8 <nicoastancai@gmail.com>
2022-10-13 17:05:09 +0300

    CreateProjectPage.AddNewComponent.

---
+++ components/FlipCard.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+import Image from "next/future/image";
+import NavLink from "./NavLink";
+
+import styles from "./FlipCard.module.scss";
+
+function FlipCard(props) {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  function flip() {
+    setIsFlipped(!isFlipped);
+  }
+  return (
+    <div className={styles.container} onClick={flip}>
+      <div
+        className={`${styles.card} ${`styles.${props.name}`} ${
+          isFlipped ? styles.isFlipped : ""
+        }`}
+      >
+        <Image
+          className={`${styles.card_front} ${styles.card_face}`}
+          src={props.source}
+          alt="LogoProiect"
+        />
+        <div
+          className={`${styles.card_back} ${styles.card_face}`}
+          style={props.styles}
+        >
+          <NavLink href={`${props.page}`} className={styles.navlink}>
+            {props.name}
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FlipCard;
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -10,6 +10,17 @@ import styles from "./projects.module.scss";
 // COMPONENTS
 import NavBar from "../../components/NavBar";
 import ThemeButton from "../../components/ThemeButton";
+import FlipCard from "../../components/FlipCard";
+
+// IMAGES
+import boutique from "../../images/projects/boutique/boutique.png";
+import culturall from "../../images/projects/culturall/culturall.png";
+import industrial from "../../images/projects/industrial/industrial.png";
+import landmark from "../../images/projects/landmark/landmark.png";
+import manufacturat from "../../images/projects/manufacturat/manufacturat.png";
+import skyscraper from "../../images/projects/skyscraper/skyscraper.png";
+
+// STYLES
 
 function MyPortfolioPage() {
   const [theme, setTheme] = useLocalStorage("theme", "light");
@@ -27,12 +38,49 @@ function MyPortfolioPage() {
         <title>Proiecte</title>
       </Head>
       <NavBar />
-      <h1 className={styles.title}>Proiecte</h1>
       <ThemeButton
         styles={projectsStyle}
         theme={theme}
         handleClick={changeTheme}
       />
+      <main className={styles.projects_box}>
+        <FlipCard
+          name={"culturall"}
+          source={culturall}
+          styles={{ backgroundColor: "#9e5a4c" }}
+          page={"about"}
+        />
+        <FlipCard
+          name={"boutique"}
+          source={boutique}
+          styles={{ backgroundColor: "#748867" }}
+          page={"about"}
+        />
+        <FlipCard
+          name={"manufacturat"}
+          source={manufacturat}
+          styles={{ backgroundColor: "#ba9569" }}
+          page={"about"}
+        />
+        <FlipCard
+          name={"landmark"}
+          source={landmark}
+          styles={{ backgroundColor: "#566c57" }}
+          page={"about"}
+        />
+        <FlipCard
+          name={"skyscraper"}
+          source={skyscraper}
+          styles={{ backgroundColor: "#486478" }}
+          page={"about"}
+        />
+        <FlipCard
+          name={"industrial"}
+          source={industrial}
+          styles={{ backgroundColor: "#ac6c53" }}
+          page={"about"}
+        />
+      </main>
     </div>
   );
 }
