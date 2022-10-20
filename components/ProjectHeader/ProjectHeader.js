5b51db408639aa7d7b9e20b3f4e79a37bf1e8e0e
NicoA8 <nicoastancai@gmail.com>
2022-10-20 14:35:49 +0300

    ProjectHeaderResponsive

--- components/ProjectHeader/ProjectHeader.js
+++ components/ProjectHeader/ProjectHeader.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Image from "next/future/image";
 
 import styles from "./ProjectHeader.module.scss";
 
 export default function ProjectHeader(props) {
+  const imageRef = useRef();
+
   const { projectName } = props;
   return (
     <header className={`${styles.header}  ${styles[projectName]}`}>
-      <div className={styles.scroll_container}>
+      <div className={styles.scroll_container} ref={imageRef}>
         <Image
           src={`${props.coverSource}`}
           className={styles.cover}
@@ -16,18 +18,20 @@ export default function ProjectHeader(props) {
           alt="cover"
         />
       </div>
-      <div className={styles.title}>
-        <p className={styles.one}>{props.title}</p>
-        <p className={styles.two}>{projectName}</p>
-      </div>
       <div className={styles.description}>
-        <Image
-          src={`${props.logoSource}`}
-          className={styles.logo}
-          width="400"
-          height="400"
-          alt="logo"
-        />
+        <div className={styles.logo_title}>
+          <div className={styles.title_box}>
+            <p className={styles.subtitle}>{props.title}</p>
+            <p className={styles.title}>{projectName}</p>
+          </div>
+          <Image
+            src={`${props.logoSource}`}
+            className={styles.logo}
+            width="400"
+            height="400"
+            alt="logo"
+          />
+        </div>
         <p className={styles.one}>{props.subtitle}</p>
         <p className={styles.two}>{props.text}</p>
       </div>
