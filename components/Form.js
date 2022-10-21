25987cb69dfaaa5481dd84d9033ab4f22e31b35c
Mihaela Halip <105174803+hmihaella@users.noreply.github.com>
2022-10-21 22:48:08 +0300

    Changes

--- components/Form.js
+++ components/Form.js
@@ -1,20 +1,41 @@
-import css from '../styles/Form.module.css'
+import styles from "../styles/Form.module.css";
+import Image from "next/image";
+import { useState } from "react";
 
-export default function Form() {
-    return(
-        <div className={css.container}>
+const Featured = () => {
+  const [index, setIndex] = useState(0);
+  const images = [
+    "/img/featured.png",
+    "/img/featured2.png",
+    "/img/featured3.png",
+  ];
 
-            <div className={css.contact}>
-                
-                <div className={css.left}></div>
-                <div className={css.right}>
-                    <p class="title">Contact Us</p>
-                    <input type="email" class="field" placeholder="Your Email"/>
-                    <input type="email" class="field" placeholder="Your Email"/>
-                    <input type="text" class="field" placeholder="Your Phone"/>
-                    <textarea class="field" placeholder="Message"></textarea>
-                </div>
-            </div>
-        </div>
-    )
-}
+  const handleArrow = (direction) =>{
+      if(direction==="l"){
+          setIndex(index !== 0 ? index-1 : 2)
+      }
+      if(direction==="r"){
+          setIndex(index !== 2 ? index+1 : 0)
+      }
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.arrowContainer} style={{ left: 0 }} onClick={()=>handleArrow("l")}>
+        <Image src="/img/arrowl.png" alt="" layout="fill" objectFit="contain"/>
+      </div>
+      <div className={styles.wrapper} style={{transform:`translateX(${-100*index}vw)`}}>
+        {images.map((img, i) => (
+          <div className={styles.imgContainer} key={i}>
+            <Image src={img} alt="" layout="fill" objectFit="contain" />
+          </div>
+        ))}
+      </div>
+      <div className={styles.arrowContainer} style={{ right: 0 }} onClick={()=>handleArrow("r")}>
+        <Image src="/img/arrowr.png" layout="fill" alt="" objectFit="contain"/>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
