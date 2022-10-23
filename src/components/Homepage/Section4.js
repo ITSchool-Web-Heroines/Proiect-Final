0ecb1ec23b5924258caa699925e4b15c3ff989f4
Andreea2194 <36506524+Andreea2194@users.noreply.github.com>
2022-10-23 22:05:57 +0300

    First Commit

--- src/components/Homepage/Section4.js
+++ src/components/Homepage/Section4.js
@@ -16,25 +16,25 @@ const Section4 = () => {
             <div class="row">
                 <div class="col">
                     <img src={Picture} />
-                    <img class="icon" src={Icon} />
+                    <img class="iconhome" src={Icon} />
                     <h3>Our clean promise</h3>
                     <p>We say “yes, please” to powerful, effective and clean ingredients and “no thank you” to yuck-worthy additives</p>
                 </div>
                 <div class="col">
                     <img src={Picture2} />
-                    <img class="icon" src={Icon2} />
+                    <img class="iconhome" src={Icon2} />
                     <h3>Supercharged performance</h3>
                     <p>Vivid color. Instant impact. Sensorial textures. Wear that won’t let up.</p>
                 </div>
                 <div class="col">
                     <img src={Picture3} />
-                    <img class="icon" src={Icon3} />
+                    <img class="iconhome" src={Icon3} />
                     <h3>Powered by superfoods</h3>
                     <p>Pure. Efficacious. Superfoods for your skin. We think of them as nature’s overachievers</p>
                 </div>
                 <div class="col">
                     <img src={Picture4} />
-                    <img class="icon" src={Icon4} />
+                    <img class="iconhome" src={Icon4} />
                     <h3>Vegan & Cruelty free</h3>
                     <p>We never, ever test on animals. Period. And, we never use animal-derived ingredients</p>
                 </div>
