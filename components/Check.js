f42552a5f25c8949ae6e3b3939d5a0d8bf8de56c
Halip Mihaela <hlpmihaela@yahoo.com>
2022-10-26 00:08:43 +0300

    Update Check.js

--- components/Check.js
+++ components/Check.js
@@ -6,11 +6,17 @@ import C1 from '../images/C1.jpg'
 export default function Check () {
     return(
         <div className={css.card}>
-            <Image src={C1} alt=""/>
-                <div className={css.container}>
-                    <h4><b>Thanks for filling out our form!</b></h4>
-                </div>
-        </div>
+            <form
+            name="contact"
+            action="/pages/success"
+            method="POST"
+            data-netlify="true"> 
+            </form>
+        <Image src={C1} alt=""/>
+            <div className={css.container}>
+                <h4><b>Thanks for filling out our form!</b></h4>
+            </div>
+    </div>
 
     )
 }
