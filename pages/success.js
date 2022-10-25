1309c86bb35da1024ba7e28913a64d9f927f9fbe
Halip Mihaela <hlpmihaela@yahoo.com>
2022-10-25 23:53:54 +0300

    Update success.js

--- pages/success.js
+++ pages/success.js
@@ -1,18 +1,17 @@
 import Layout from '../components/Layout'
-import Check from '../components/Check'
+import Success from '../components/Check'
 import css from '../styles/Home.module.css'
 
-export default function Success () {
-   return (
-    <Layout>
-
-    <div className={css.container}>
-        <main>
-            <Check/>
-        </main>
-    </div>
-
-    </Layout>
+export default function Check() {
+    return (
+      <Layout>
+  
+         <div className={css.container}>
+          <Success/>
+      </div>
+  
+      </Layout>
+     
     )
 }
 
