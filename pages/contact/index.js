25d84e39f31ea6b03eac4184cdf5c15794b27bee
NicoA8 <nicoastancai@gmail.com>
2022-10-25 18:32:21 +0300

    AddForm.AddEmailJs

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,10 +1,10 @@
 import React, { useEffect } from "react";
+import emailjs from "@emailjs/browser";
 import Head from "next/head";
-import { useState } from "react";
+import { useContext, useRef } from "react";
 
 // CONTEXT
 import { ThemeContext } from "../../context/theme";
-import { useContext } from "react";
 
 // STYLES
 import styles from "./contact.module.scss";
@@ -18,14 +18,52 @@ import Signature from "../../components/Signature/Signature";
 
 function MyContactPage() {
   const theme = useContext(ThemeContext);
+  const form = useRef();
 
+  const sendEmail = e => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_qdj0kzo",
+        "template_wmbecdr",
+        form.current,
+        "z4CvshztpUke0xKoi"
+      )
+      .then(
+        result => {
+          console.log(result.text);
+          console.log("message sent");
+        },
+        error => {
+          console.log(error.text);
+          console.log("message not sent");
+        }
+      );
+  };
   return (
     <LoadingWrap title={"Contact"}>
       <Head>
         <title>Contact</title>
       </Head>
       <NavBar />
-      <main></main>
+      <main>
+        <form className={styles.form} ref={form} onSubmit={sendEmail}>
+          <label className={styles.form_title}>
+            Nume
+            <input className={styles.name} type="text" name="user_name" />
+          </label>
+          <label className={styles.form_title}>
+            Email
+            <input className={styles.email} type="email" name="user_email" />
+          </label>
+          <label className={styles.form_title}>
+            Mesaj
+            <textarea className={styles.message} name="message" />
+          </label>
+          <input className={styles.button} type="submit" value="Send" />
+        </form>
+      </main>
       <FooterAndBackTop />
       <CustomParticles color={theme.isDark ? "#fff" : "#000"} />
     </LoadingWrap>
