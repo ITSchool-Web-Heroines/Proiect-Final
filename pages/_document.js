6c2601a1e38cfeed854da5ea8da51c1cf3d65497
anamariap21 <anapacurar321@gmail.com>
2022-10-26 22:24:45 +0300

    commit

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,15 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Link from 'next/link';
+import Link from 'next/link'
 
 export default function Document() {
   return (
     <Html>
       <Head>
         <Link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
           rel="stylesheet"
-        />
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+
+        ></Link>
         <Link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
