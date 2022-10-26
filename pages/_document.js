140bb5688d2afda4c45cbe5663ef61aabe6af344
anamariap21 <anapacurar321@gmail.com>
2022-10-26 22:28:48 +0300

    commit

--- pages/_document.js
+++ pages/_document.js
@@ -5,19 +5,14 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <Link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
-
+        <Link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" 
         ></Link>
-        <Link
+        <Link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+        rel="stylesheet">
+          </Link>
+        <Link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"
           rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
-        ></Link>
-        <Link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"
-        ></Link>
+          ></Link>
       </Head>
       <body>
         <Main />
