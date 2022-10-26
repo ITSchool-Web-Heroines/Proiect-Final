ecc6f8899eaf788c113d61df9abcb9eda88a39ee
anamariap21 <anapacurar321@gmail.com>
2022-10-26 22:31:00 +0300

    commit

--- pages/_document.js
+++ pages/_document.js
@@ -1,19 +1,22 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Link from 'next/link'
 
 export default function Document() {
   return (
     <Html>
-      <Head>
-        <Link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" 
-        ></Link>
-        <Link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
-        rel="stylesheet">
-          </Link>
-        <Link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"
-          rel="stylesheet"
-          ></Link>
-      </Head>
+      <Head />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+        rel="stylesheet"
+      />
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+      ></link>
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"
+      ></link>
+
       <body>
         <Main />
         <NextScript />
