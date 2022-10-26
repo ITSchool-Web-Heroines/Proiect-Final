18667abbba6d7c0ed035217dd49f0cec9227f4a6
anamariap21 <anapacurar321@gmail.com>
2022-10-26 21:33:59 +0300

    ProiectFinal

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,38 +1,29 @@
-import React from "react";
-
-const ContactForm = () => {
+export default function ContactForm() {
   return (
-    <>
-      <section className="contact" id="contact">
-
-        <div className="row">
-        <label htmlFor="name">Enter your name:
-        <form className="form-contact" name="form-contact" method="POST" data-netlify="true">
-              <p>
-                <label>Your Name: <input type="text" name="name" /></label>
-              </p>
-              <p>
-                <label>Your Email: <input type="email" name="email" /></label>
-              </p>
-              <p>
-                <label>Your Role: <select name="role[]" multiple>
-                    <option value="leader">Leader</option>
-                    <option value="follower">Follower</option>
-                  </select></label>
-              </p>
-              <p>
-                <label>Message: <textarea name="message" defaultValue={""} /></label>
-              </p>
-              <p>
-                <button type="submit">Send</button>
-              </p>
-            </form> </label>
-          <input type="text" id="name" />
-        </div>
-        
-      </section>
-    </>
-  );
-};
-
-export default ContactForm;
+    <form name="contact" method="POST">
+      <input type="hidden" name="form-name" value="contact" />
+      <p>
+<input type="text" name="firstname" id="firstname" />
+        <label htmlFor="yourname">
+          Your Name:
+        </label> <br />
+        <input type="text" name="name" id="yourname" />
+      </p>
+      <p>
+        <label htmlFor="youremail">
+          Your Email:
+        </label> <br />
+        <input type="email" name="email" id="youremail" />
+      </p>
+      <p>
+        <label htmlFor="yourmessage">
+          Message:
+        </label> <br />
+        <textarea name="message" id="yourmessage"></textarea>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+  )
+}
