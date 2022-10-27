a52804e77067a0b325b57cdf5d8c337e2bd6671d
anamariap21 <anapacurar321@gmail.com>
2022-10-27 09:56:58 +0300

    servicii

--- components/Services.js
+++ components/Services.js
@@ -10,55 +10,31 @@ const Services = () => {
           <div className="box">
             <img src={"images/serv-1.png"} alt="serviciu" />
             <h3>Rezultate garantate</h3>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Exercitationem, delectus!
-            </p>
           </div>
 
           <div className="box">
             <img src={"images/serv-2.png"} alt="serviciu" />
             <h3>Oferte Personalizate</h3>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Exercitationem, delectus!
-            </p>
           </div>
 
           <div className="box">
             <img src={"images/serv-3.png"} alt="serviciu" />
             <h3>Deadline-uri respectate</h3>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Exercitationem, delectus!
-            </p>
           </div>
 
           <div className="box">
             <img src={"images/serv-4.png"} alt="serviciu" />
             <h3>Strategii Moderne de MK</h3>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Exercitationem, delectus!
-            </p>
           </div>
 
           <div className="box">
             <img src={"images/serv-5.png"} alt="serviciu" />
             <h3>Strategii inovative</h3>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Exercitationem, delectus!
-            </p>
           </div>
 
           <div className="box">
             <img src={"images/serv-6.png"} alt="serviciu" />
             <h3>Echipa Dedicata</h3>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Exercitationem, delectus!
-            </p>
           </div>
         </div>
       </section>
