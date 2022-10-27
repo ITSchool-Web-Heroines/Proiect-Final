dc2e612bf2d0a2ee42903a36f968c34fb2359233
Belei Bianca <biebianca99@gmail.com>
2022-10-27 03:09:04 +0300

    Modificari

--- components/FormComanda/FormComanda.jsx
+++ components/FormComanda/FormComanda.jsx
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import emailjs from "emailjs-com";
 import styles from "./formComanda.module.css";
-
+import Popup from "../Popup/Popup";
 import Image from "next/future/image";
 import Poster from "../../images/poster.jpg";
 
 export default function FormComanda() {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_thw9wxe",
+        "template_alei8kv",
+        form.current,
+        "ccobHoXEpnce64hgy"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
+  const [popup, setPopup] = useState(false);
   return (
     <div className={styles.container}>
       <div className={styles.all}>
         <form
           className={styles.form}
           name="comanda"
-          method="POST"
-          data-netlify="true"
-          onSubmit="submit"
+          ref={form}
+          onSubmit={sendEmail}
         >
           <h2 className={styles.title}>Adresă de livrare</h2>
           <div className={styles.two}>
@@ -23,6 +45,7 @@ export default function FormComanda() {
                 name="nume"
                 placeholder="Nume"
                 className={styles.input}
+                required
               ></input>
             </div>
             <div className={styles.label_input}>
@@ -30,6 +53,7 @@ export default function FormComanda() {
                 className={styles.input}
                 name="prenume"
                 placeholder="Prenume"
+                required
               ></input>
             </div>
           </div>
@@ -38,6 +62,7 @@ export default function FormComanda() {
               className={styles.input}
               name="adresa"
               placeholder="Adresă"
+              required
             ></input>
           </div>
           <div className={styles.two}>
@@ -47,6 +72,7 @@ export default function FormComanda() {
                 id="localitate"
                 name="localitate"
                 placeholder="Localitate"
+                required
               ></input>
             </div>
             <div className={styles.label_input}>
@@ -54,6 +80,7 @@ export default function FormComanda() {
                 className={styles.input}
                 name="judet"
                 placeholder="Județ"
+                required
               ></input>
             </div>
           </div>
@@ -62,6 +89,7 @@ export default function FormComanda() {
               className={styles.input}
               name="telefon"
               placeholder="Telefon"
+              required
             ></input>
           </div>
           <h2 className={styles.title}>Metodă de expediere</h2>
@@ -79,10 +107,17 @@ export default function FormComanda() {
             <label for="plata">Plata ramburs</label>
           </div>
           <div>
-            <button type="submit" className={styles.btn}>
+            <button
+              type="submit"
+              className={styles.btn}
+              onClick={() => setPopup(true)}
+            >
               Trimite comanda
             </button>
           </div>
+          <Popup trigger={popup} setTrigger={setPopup}>
+            <h2 className={styles.popup_text}>Comanda a fost trimisă!</h2>
+          </Popup>
         </form>
         <div>
           <Image src={Poster} alt="poster" className={styles.poster} />
--- components/FormContact/FormContact.jsx
+++ components/FormContact/FormContact.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import emailjs from "emailjs-com";
 import styles from "./formContact.module.css";
-
+import Popup from "../Popup/Popup";
 import Image from "next/future/image";
 import FormImg from "../../images/bck4.png";
 
 export default function FormContact() {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_thw9wxe",
+        "template_kif8y1l",
+        e.target,
+        "ccobHoXEpnce64hgy"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
+  const [popup, setPopup] = useState(false);
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Scrie-ne un mesaj</h1>
@@ -12,17 +35,26 @@ export default function FormContact() {
         <form
           className={styles.form}
           name="contact"
-          method="POST"
-          data-netlify="true"
-          onSubmit="submit"
+          ref={form}
+          onSubmit={sendEmail}
         >
           <label className={styles.label}>
             Nume
-            <input type="text" name="user_name" className={styles.input} />
+            <input
+              type="text"
+              name="user_name"
+              className={styles.input}
+              required
+            />
           </label>
           <label className={styles.label}>
             Email
-            <input type="email" name="user_email" className={styles.input} />
+            <input
+              type="email"
+              name="user_email"
+              className={styles.input}
+              required
+            />
           </label>
 
           <label className={styles.label}>
@@ -39,11 +71,18 @@ export default function FormContact() {
               ></textarea>
             </label>
             <div className={styles.btn_div}>
-              <button type="submit" className={styles.btn}>
+              <button
+                type="submit"
+                className={styles.btn}
+                onClick={() => setPopup(true)}
+              >
                 Trimite
               </button>
             </div>
           </div>
+          <Popup trigger={popup} setTrigger={setPopup}>
+            <h2 className={styles.popup_text}>Mesajul a fost trimis!</h2>
+          </Popup>
         </form>
         <Image src={FormImg} alt="image" className={styles.img} />
       </div>
